adb8db9446fa5f72e21a3af78f4ed4b68cef3943
Seth Etter <sethetter@gmail.com>
2016-12-12 11:19:25 -0600

    Active Documents panel loaded with data!
    Ref T107

--- resources/views/partials/home/active-documents.blade.php
+++ resources/views/partials/home/active-documents.blade.php
@@ -5,27 +5,37 @@
     <div class="recent-activity">
         <h3>{{ trans('messages.recentactivity') }}</h3>
 
-        <!-- TODO: Loop through recently active docs here -->
-        <article>
-            <a href="#" class="document-title">A Document Title!</a>
-            <span class="date">
-                {{ trans('messages.updated') }} 1 million days ago
-            </span>
-        </article>
+        @foreach ($mostRecentDocuments as $recentDocument)
+            <article>
+                <a href="/documents/{{ $recentDocument['slug'] }}"
+                    class="document-title" title="{{ $recentDocument['title'] }}">
+                    {{ $recentDocument['title'] }}
+                </a>
+                <span class="date">
+                    {{ trans('messages.updated') }}
+                    {{ Carbon\Carbon::parse($recentDocument['updated_at'])->diffForHumans() }}
+                </span>
+            </article>
+        @endforeach
     </div>
 
     <div class="move-active">
         <h3>{{ trans('messages.mostactive') }}</h3>
 
         <!-- TODO: Loop through most active docs here -->
-        <article>
-            <a href="#" class="document-title">A Document Title!</a>
-            <div class="document-stats">
-                <ul>
-                    <li>3 {{ trans('messages.document.comments') }}</li>
-                    <li>2 {{ trans('messages.document.notes') }}</li>
-                </ul>
-            </div>
-        </article>
+        @foreach ($mostActiveDocuments as $activeDocument)
+            <article>
+                <a href="/documents/{{ $activeDocument['slug'] }}"
+                    class="document-title" title="{{ $activeDocument['title'] }}">
+                    {{ $activeDocument['title'] }}
+                </a>
+                <div class="document-stats">
+                    <ul>
+                        <li>{{ $activeDocument['comment_count'] }} {{ trans('messages.document.comments') }}</li>
+                        <li>{{ $activeDocument['note_count'] }} {{ trans('messages.document.notes') }}</li>
+                    </ul>
+                </div>
+            </article>
+        @endforeach
     </div>
 </section>
