7263cd7a6ef8e84d0b2068017c3a526f0a069eea
Tanner Doshier <git@doshitan.com>
2016-12-16 17:08:10 -0600

    Basic document editing
    Summary:
    Also adds a very basic public document viewing page. Tweaks how we handle
    featured images. Does not include the document "dates" stuff, because is that
    even used? Also does not include the comment export links or statistics stuff as
    those are separate to actually editing a document.
    
    Test Plan:
    - Run migrations, these are the least tested aspect, so something might break
    - Try to edit a document
    
    Reviewers: sethetter
    
    Maniphest Tasks: T131
    
    Differential Revision: https://phabricator.opengovfoundation.org/D61

--- resources/views/documents/edit.blade.php
+++ resources/views/documents/edit.blade.php
@@ -14,7 +14,7 @@
 
         {{ Form::mInput('checkbox', 'featured', trans('messages.document.featured'), null, request()->user()->isAdmin() ? [] : ['disabled' => true]) }}
         {{ Form::mInput('file', 'featured-image', trans('messages.document.featured_image'), null, request()->user()->isAdmin() ? [] : ['disabled' => true]) }}
-        @if ($document->thumbnail)
+        @if ($document->featuredImage)
             <img src="{{ $document->getFeaturedImageUrl() }}"/>
 
             {{-- Submits the hidden remove image form --}}
@@ -89,7 +89,7 @@
 
     {{-- Hidden form to delete featured image of document --}}
     {{ Form::open([
-           'route' => ['documents.images.destroy', $document->slug, $document->thumbnail],
+           'route' => ['documents.images.destroy', $document->slug, $document->featuredImage],
            'method' => 'delete',
            'style' => 'display: none;',
            'id' => 'remove-featured-image-form',
