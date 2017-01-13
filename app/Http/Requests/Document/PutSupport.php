3f7dcb16fb9c0822ffaca8f6ac3510f04d70efec
Seth Etter <sethetter@gmail.com>
2017-01-13 14:54:56 -0600

    Fixes from a bad merge

---
+++ app/Http/Requests/Document/PutSupport.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Document;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PutSupport extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (!$this->user()) {
+            return $this->redirect()->route('login');
+        }
+
+        $document = null;
+
+        foreach (['document', 'documentTrashed'] as $key) {
+            if (!empty($this->route()->parameter($key))) {
+                $document = $this->route()->parameter($key);
+                break;
+            }
+        }
+
+        return $document && $document->canUserEdit($this->user());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
