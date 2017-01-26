970806544877aad529f4ba5f67cdbb2145ec5375
Tanner Doshier <git@doshitan.com>
2017-01-26 10:19:30 -0600

    Add notification preference relationship to user model and make use of it

--- app/Models/NotificationPreference.php
+++ app/Models/NotificationPreference.php
@@ -14,6 +14,7 @@ class NotificationPreference extends Model
     const TYPE_TEXT = "text";
 
     protected $table = 'notification_preferences';
+    protected $fillable = ['event', 'type', 'user_id', 'sponsor_id'];
     public $timestamps = false;
 
     public function sponsor()
