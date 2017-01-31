22d550985fd6e04c73d8c9669f9486c77d0bb5a3
Tanner Doshier <git@doshitan.com>
2017-01-31 13:27:46 -0600

    Fix migration

--- database/migrations/2017_01_31_170338_user_sponsor_membership_notification.php
+++ database/migrations/2017_01_31_170338_user_sponsor_membership_notification.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddedToSponsorNotification extends Migration
+class UserSponsorMembershipNotification extends Migration
 {
     /**
      * Run the migrations.
