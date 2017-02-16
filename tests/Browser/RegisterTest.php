3fc570abbf07456412db8753e20e19e475a25857
Tanner Doshier <git@doshitan.com>
2017-02-16 15:32:53 -0600

    Register test

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use App\Mail\EmailVerification;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Mail;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testUserCanLogIn()
+    {
+        $fakeUser = factory(User::class)->make();
+
+        Event::fake();
+        Mail::fake();
+
+        $this->browse(function ($browser) use ($fakeUser) {
+            $browser->visit('/register')
+                ->type('fname', $fakeUser->fname)
+                ->type('lname', $fakeUser->lname)
+                ->type('email', $fakeUser->email)
+                ->type('password', 'secret')
+                ->type('password-confirm', 'secret')
+                ->press('Register')
+                ->assertPathIs('/');
+
+            Event::assertDispatched('Illuminate\Auth\Events\Registered');
+            Mail::assertSent(EmailVerification::class, function ($mail) use ($fakeUser)) {
+                return $mail->hasTo($fakeUser->email);
+            };
+
+            $user = User::orderBy('created_by', 'DESC')->first();
+        });
+    }
+}
