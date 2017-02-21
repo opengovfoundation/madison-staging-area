df47a1c54af9591d120c0134211770df17accc56
Tanner Doshier <git@doshitan.com>
2017-02-21 10:36:06 -0600

    Register user test and seed/factories improvements
    Summary:
    - Seed more content of varying length and amount
    - Don't crash home page if there are no documents, we all gotta start somewhere
    - Set default `MAIL_FROM`
    - Set `MAIL_DRIVER` to 'log' for tests
    - Don't create a default page of content of a document, but automatically
      make the first content model attached to a document the first page
    
    Test Plan: - `make test`
    
    Reviewers: sethetter
    
    Reviewed By: sethetter
    
    Maniphest Tasks: T201
    
    Differential Revision: https://phabricator.opengovfoundation.org/D114

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,12 +10,15 @@
 |
 */
 
+use App\Models\Annotation;
+use App\Models\AnnotationTypes;
 use App\Models\Category;
 use App\Models\Doc;
-use App\Models\Sponsor;
+use App\Models\DocContent;
 use App\Models\Page;
 use App\Models\PageContent;
 use App\Models\Role;
+use App\Models\Sponsor;
 use App\Models\User;
 
 $factory->define(User::class, function (Faker\Generator $faker) {
@@ -26,6 +29,7 @@ $factory->define(User::class, function (Faker\Generator $faker) {
         'lname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = 'secret',
+        'token' => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
@@ -57,9 +61,37 @@ $factory->define(PageContent::class, function (Faker\Generator $faker) {
 });
 
 $factory->define(Doc::class, function (Faker\Generator $faker) {
-    return [ 'title' => $faker->words(5, true) ];
+    return [
+        'title' => substr($faker->sentence(5), 0, -1),
+    ];
+});
+
+$factory->define(DocContent::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->paragraphs(100, true),
+    ];
 });
 
 $factory->define(Category::class, function (Faker\Generator $faker) {
     return [ 'name' => $faker->words(2, true) ];
+});
+
+$factory->define(Annotation::class, function (Faker\Generator $faker) {
+    return [
+    ];
+});
+
+$factory->define(AnnotationTypes\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->paragraphs($faker->numberBetween(1, 3), true),
+    ];
+});
+
+$factory->define(AnnotationTypes\Range::class, function (Faker\Generator $faker) {
+    return [
+        'start' => '/p[1]',
+        'end' => '/p[1]',
+        'start_offset' => $faker->numberBetween(1, 20),
+        'end_offset' => $faker->numberBetween(1, 20),
+    ];
 });
