49839a1cb01a692d09fcc2ea129c439e4697c164
Tanner Doshier <git@doshitan.com>
2017-02-22 14:36:32 -0600

    User password setting test
    Summary:
    Also move test page assert into the base page class.
    
    Test Plan:
    `make test`
    
    Reviewers: sethetter
    
    Spies:

--- tests/Browser/Pages/Page.php
+++ tests/Browser/Pages/Page.php
@@ -2,10 +2,16 @@
 
 namespace Tests\Browser\Pages;
 
+use Laravel\Dusk\Browser;
 use Laravel\Dusk\Page as BasePage;
 
 abstract class Page extends BasePage
 {
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url());
+    }
+
     /**
      * Get the global element shortcuts for the site.
      *
