515a0d617b0a78621b76d0911fa6369a84262d72
Seth Etter <sethetter@gmail.com>
2017-02-22 08:00:59 -0600

    Test user can create sponsor
    Summary:
    
    - Tests that a user can create a new sponsor with a default 'pending' status.
    - Also test that use must be logged in to access sponsor create page.
    
    Test Plan: - `make test`
    
    Reviewers: doshitan
    
    Subscribers:

---
+++ tests/Browser/SponsorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use App\Models\Sponsor;
+use Illuminate\Support\Facades\Event;
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\Sponsor as SponsorPages;
+use Tests\DuskTestCase;
+
+class SponsorTest extends DuskTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testUserCanCreateSponsor()
+    {
+        // TODO: currently doesn't work with Dusk
+        //Event::fake();
+
+        $attrs = factory(Sponsor::class)->make()->toArray();
+
+        $this->browse(function ($browser) use ($attrs) {
+            $browser->loginAs($this->user)
+                ->visit(new SponsorPages\CreatePage())
+                ->fillNewSponsorForm($attrs)
+                ->click('@submitBtn')
+                ;
+
+            $sponsor = Sponsor::first();
+
+            $this->assertEquals($sponsor->status, Sponsor::STATUS_PENDING);
+
+            $this->assertEquals($sponsor->name, $attrs['name']);
+            $this->assertEquals($sponsor->display_name, $attrs['display_name']);
+            $this->assertEquals($sponsor->address1, $attrs['address1']);
+            $this->assertEquals($sponsor->city, $attrs['city']);
+            $this->assertEquals($sponsor->state, $attrs['state']);
+            $this->assertEquals($sponsor->postal_code, $attrs['postal_code']);
+            $this->assertEquals($sponsor->phone, $attrs['phone']);
+
+            // TODO: currently doesn't work with Dusk
+            //Event::assertDispatched('App\Events\SponsorCreated', function ($e) use ($sponsor) {
+            //    return $e->sponsor->id === $sponsor->id;
+            //});
+        });
+    }
+}
