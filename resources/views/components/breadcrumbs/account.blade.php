fc0750490d20ab6ced6258a5acdb6dc4d672e5b3
Seth Etter <sethetter@gmail.com>
2017-03-13 10:29:27 -0500

    Add breadcrumbs to create sponsor page

--- resources/views/components/breadcrumbs/account.blade.php
+++ resources/views/components/breadcrumbs/account.blade.php
@@ -4,13 +4,26 @@
             @lang('messages.user.settings_pages.account')
         </a>
     </li>
-    @if (strpos(Request::route()->uri, 'sponsors'))
-        <li class="active">@lang('messages.sponsor.list')</li>
+    @if (strpos(Request::route()->uri, 'sponsors') !== false)
+        @if (strpos(Request::route()->uri, 'create'))
+            <li>
+                <a href="{{ route('users.sponsors.index', Auth::user()) }}">
+                    @lang('messages.sponsor.list')
+                </a>
+            </li>
+            <li class="active">@lang('messages.new')</li>
+        @else
+            <li class="active">@lang('messages.sponsor.list')</li>
+        @endif
+
     @elseif (strpos(Request::route()->uri, 'account'))
         <li class="active">@lang('messages.settings')</li>
+
     @elseif (strpos(Request::route()->uri, 'password'))
         <li class="active">@lang('messages.user.settings_pages.password')</li>
+
     @elseif (strpos(Request::route()->uri, 'notifications'))
         <li class="active">@lang('messages.user.settings_pages.notifications')</li>
+
     @endif
 </ol>
