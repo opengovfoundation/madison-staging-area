85ff6566f632a7bd6a0877f3fa08e7f75aa6fe21
Seth Etter <sethetter@gmail.com>
2017-03-22 08:52:52 -0500

    Unnecessary comment cleanup

--- resources/views/components/breadcrumbs/account.blade.php
+++ resources/views/components/breadcrumbs/account.blade.php
@@ -12,19 +12,14 @@
         </li>
     @endif
     <li class="active">
-        {{--sponsors.create--}}
         @if (Route::currentRouteName() === 'sponsors.create')
             @lang('messages.new')
-        {{--users.sponsors.index--}}
         @elseif (Route::currentRouteName() === 'users.sponsors.index')
             @lang('messages.sponsor.list')
-        {{--users.settings.account.edit--}}
         @elseif (Route::currentRouteName() === 'users.settings.account.edit')
             @lang('messages.settings')
-        {{--users.settings.password.edit--}}
         @elseif (Route::currentRouteName() === 'users.settings.password.edit')
             @lang('messages.user.settings_pages.password')
-        {{--users.settings.notifications.edit--}}
         @elseif (Route::currentRouteName() === 'users.settings.notifications.edit')
             @lang('messages.user.settings_pages.notifications')
         @endif
