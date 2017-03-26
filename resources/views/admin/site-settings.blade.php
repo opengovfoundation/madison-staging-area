c4958bb2e76760eea95f4afac139742fa5da4781
Seth Etter <sethetter@gmail.com>
2017-03-26 13:49:34 -0500

    Site settings and custom pages UX

--- resources/views/admin/site-settings.blade.php
+++ resources/views/admin/site-settings.blade.php
@@ -15,38 +15,58 @@
 
         <div class="col-md-9">
             {{ Form::model($currentSettings, ['route' => ['admin.site.update'], 'method' => 'put']) }}
-                @foreach ($allSettingsDesc as $key => $desc)
-                    @if ($desc['type'] === 'select')
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">@lang('messages.admin.date_time_settings_title')</h3>
+                    </div>
+                    <div class="panel-body">
                         {{ Form::mSelect(
-                                $key,
-                                trans('messages.admin.'.$key),
-                                $options[$key]['choices'],
+                                'madison.date_format',
+                                trans('messages.admin.madison.date_format'),
+                                $options['madison.date_format']['choices'],
                                 null,
                                 [],
-                                trans('messages.admin.'.$key.'_help') !== 'messages.admin.'.$key.'_help'
-                                       ? trans('messages.admin.'.$key.'_help')
+                                trans('messages.admin.madison.date_format_help') !== 'messages.admin.madison.date_format_help'
+                                       ? trans('messages.admin.madison.date_format_help')
                                        : null
                                 )
                         }}
-                    @elseif ($desc['type'] === 'text')
+                        {{ Form::mSelect(
+                                'madison.time_format',
+                                trans('messages.admin.madison.time_format'),
+                                $options['madison.time_format']['choices'],
+                                null,
+                                [],
+                                trans('messages.admin.madison.time_format_help') !== 'messages.admin.madison.time_format_help'
+                                       ? trans('messages.admin.madison.time_format_help')
+                                       : null
+                                )
+                        }}
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">@lang('messages.admin.google_analytics_settings_title')</h3>
+                    </div>
+                    <div class="panel-body">
                         {{ Form::mInput(
                                 'text',
-                                $key,
-                                trans('messages.admin.'.$key),
+                                'madison.google_analytics_property_id',
+                                trans('messages.admin.madison.google_analytics_property_id'),
                                 null,
                                 [ 'placeholder' =>
-                                    !empty($options[$key]['placeholder'])
-                                        ? $options[$key]['placeholder']
+                                    !empty($options['madison.google_analytics_property_id']['placeholder'])
+                                        ? $options['madison.google_analytics_property_id']['placeholder']
                                         : ''
                                 ],
-                                trans('messages.admin.'.$key.'_help') !== 'messages.admin.'.$key.'_help'
-                                       ? trans('messages.admin.'.$key.'_help')
+                                trans('messages.admin.madison.google_analytics_property_id_help') !== 'messages.admin.madison.google_analytics_property_id_help'
+                                       ? trans('messages.admin.madison.google_analytics_property_id_help')
                                        : null
                                 )
-                        }}
-                    @endif
-                @endforeach
 
+                        }}
+                    </div>
+                </div>
                 {{ Form::mSubmit() }}
             {{ Form::close() }}
         </div>
