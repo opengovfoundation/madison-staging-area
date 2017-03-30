12cff348f4e02df44b94908afd25aecdd3b83d83
Tanner Doshier <git@doshitan.com>
2017-03-30 15:58:22 -0500

    WIP

--- app/Http/Controllers/SponsorMemberController.php
+++ app/Http/Controllers/SponsorMemberController.php
@@ -14,6 +14,16 @@ use App\Events\SponsorMemberRoleChanged;
 class SponsorMemberController extends Controller
 {
     /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
