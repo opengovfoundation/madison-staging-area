3064372f04c74f858eeafa4b434f5e29c0ac3a91
Seth Etter <sethetter@gmail.com>
2017-04-10 11:34:37 -0500

    Put the "Query" button back in

--- resources/views/documents/list.blade.php
+++ resources/views/documents/list.blade.php
@@ -4,10 +4,84 @@
 
 @section('content')
     <div class="page-header">
+        <button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#queryModal">Query</button>
         <h1>{{ trans('messages.document.list') }}</h1>
     </div>
 
     @include('components.errors')
+
+
+    <div class="modal fade" id="queryModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Query</h4>
+                </div>
+                <div class="modal-body">
+                    {{ Form::open(['route' => 'documents.index', 'method' => 'get']) }}
+                        {{ Form::mInput('text', 'q', trans('messages.search.title')) }}
+                        {{ Form::mSelect(
+                               'sponsor_id[]',
+                               trans('messages.document.sponsor'),
+                               $sponsors->mapWithKeys_v2(function ($item) {return [$item->id => $item->display_name]; })->toArray(),
+                               null,
+                               ['multiple' => true]
+                               )
+                        }}
+                        @if (Auth::user())
+                            {{ Form::mSelect(
+                                   'publish_state[]',
+                                   trans('messages.document.publish_state'),
+                                   collect($publishStates)->mapWithKeys_v2(function ($item) {return [$item => trans('messages.document.publish_states.'.$item)]; })->toArray(),
+                                   null,
+                                   ['multiple' => true]
+                                   )
+                            }}
+                        @endif
+                        {{ Form::mSelect(
+                               'discussion_state[]',
+                               trans('messages.document.discussion_state'),
+                               collect($discussionStates)->mapWithKeys_v2(function ($item) {return [$item => trans('messages.document.discussion_states.'.$item)]; })->toArray(),
+                               null,
+                               ['multiple' => true]
+                               )
+                        }}
+                        {{ Form::mSelect(
+                               'order',
+                               trans('messages.order_by'),
+                               [
+                                   'created_at' => trans('messages.created'),
+                                   'updated_at' => trans('messages.updated'),
+                                   'title' => trans('messages.document.title'),
+                                   'activity' => trans('messages.document.activity'),
+                                   'relevance' => trans('messages.relevance'),
+                               ])
+                        }}
+                        {{ Form::mSelect(
+                               'order_dir',
+                               trans('messages.order_by_direction'),
+                               [
+                                   'DESC' => trans('messages.order_by_dir_desc'),
+                                   'ASC' => trans('messages.order_by_dir_asc')
+                               ])
+                        }}
+                        {{ Form::mSelect(
+                               'limit',
+                               trans('messages.limit'),
+                               array_combine($range = [10, 25, 50], $range)
+                               )
+                        }}
+
+                        <button type="button" class="btn btn-default" data-dismiss="modal">@lang('messages.close')</button>
+                        <a href="{{ request()->url() }}" class="btn btn-default">@lang('messages.clear')</a>
+                        {{ Form::mSubmit() }}
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     @foreach ($documents as $document)
         <div class="row document-row">
